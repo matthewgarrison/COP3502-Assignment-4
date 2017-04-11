09ef32da933e1178a855e5e6933abd8349fdeb45
njones86 <njones86@knights.ucf.edu>
2017-04-11 10:27:23 -0400

    Add files via upload
    updated structures
    added insert / print <- still buggy

--- segfault_stars_heap.c
+++ segfault_stars_heap.c
@@ -4,33 +4,85 @@
 
 int main() {
 
+    heap_ptr heap = heap_create();
+    int data;
+    for(int i=0; i<10; i++){
+        scanf("%d", &data);
+        heap_add(heap->root_list, data);
+        heap_print(heap->root_list);
+        printf("\n");
+    }
+
+    heap_print(heap->root_list);
+    printf("\n");
 
     return 0;
 }
 
-heap_node_t * heap_node_create(int val){
-    heap_node_t * new_node = (heap_node_t*)malloc(sizeof(heap_node_t));
+heap_node_ptr heap_node_create(int val){
+    heap_node_ptr new_node = (heap_node_ptr)malloc(sizeof(heap_node_t));
     new_node -> val = val;
     new_node -> children = list_create();
+    new_node -> next = NULL;
     return new_node;
 }
 
-heap_t * heap_create() {
-    heap_t * new_heap = (heap_t*)malloc(sizeof(heap_t));
+heap_ptr heap_create() {
+    heap_ptr new_heap = (heap_ptr)malloc(sizeof(heap_t));
     // We'll never access the root node's value, so it doesn't matter what it is.
-    new_heap -> root = heap_node_create(-1);
+    new_heap -> root_list = list_create();
     return new_heap;
 }
 
-list_node_t * list_node_create(heap_node_t * val) {
-    list_node_t * new_node = (list_node_t*)malloc(sizeof(list_node_t));
-    new_node -> val = val;
-    return new_node;
-}
+// list_node_t * list_node_create(heap_node_t * val) {
+//     list_node_t * new_node = (list_node_t*)malloc(sizeof(list_node_t));
+//     new_node -> val = val;
+//     return new_node;
+// }
 
-list_t * list_create() {
-    list_t * new_list = (list_t*)malloc(sizeof(list_t));
+heap_list_ptr list_create() {
+    heap_list_ptr new_list = (heap_list_ptr)malloc(sizeof(heap_list_t));
     new_list -> head = NULL;
     new_list -> tail = NULL;
     return new_list;
+}
+
+void heap_add(heap_list_ptr curList, int value){
+    if(curList->head == NULL){
+        if(DEBUG)
+            printf("empty list\n");
+        curList->head = heap_node_create(value);
+        curList->tail = curList->head;
+        return;
+    }
+    heap_node_ptr curNode = curList->head;
+    while(curNode->next != NULL && curNode->val > value){
+        curNode = curNode->next;
+    }
+    if(curNode->val > value){
+        printf("going down\n");
+        heap_add(curNode->children, value);
+        return;        
+    }
+    else{
+        if(DEBUG)
+            printf("reached end of list\n");
+        curNode->next = heap_node_create(value);
+        curList->tail = curNode->next;
+    }
+}
+
+void heap_print(heap_list_ptr curList){
+    if(curList->head==NULL){
+        printf("(!!)");
+        return;
+    }
+    heap_node_ptr curNode = curList->head;
+    printf("(");
+    while(curNode != NULL){
+        printf(" %d", curNode->val);
+        heap_print(curNode->children);
+        curNode = curNode->next;
+    }
+    printf(")");
 }
--- segfault_stars_heap.h
+++ segfault_stars_heap.h
@@ -1,29 +1,40 @@
 #ifndef SEGFAULT_STARS_H
 #define SEGFAULT_STARS_H
 
-struct heap_node;
+// struct heap_list;
 
-typedef struct list_node {
-	struct heap_node * val;
-	struct list_node * next;
-} list_node_t;
+// typedef struct list_node {
+// 	struct heap_node * val;
+// 	struct list_node * next;
+// } list_node_t;
 
-typedef struct {
-	list_node_t * head, * tail;
-} list_t;
+#define DEBUG 1
 
 typedef struct heap_node {
 	int val;
-	list_t * children;
+	struct heap_list * children; //level below current node
+	struct heap_node * next;		//same level as current node
 } heap_node_t;
 
+typedef heap_node_t * heap_node_ptr;
+
+typedef struct heap_list{
+	heap_node_t * head, * tail;
+} heap_list_t;
+
+typedef heap_list_t * heap_list_ptr;
+
 typedef struct {
-	heap_node_t * root;
+	heap_list_t * root_list;
 } heap_t;
 
-heap_node_t * head_node_create();
-heap_t * heap_create();
-list_node_t * list_node_create();
-list_t * list_create();
+typedef heap_t * heap_ptr;
+
+heap_node_ptr head_node_create(int val);
+heap_ptr heap_create();
+// list_node_t * list_node_create();
+heap_list_ptr list_create();
+void heap_add(heap_list_ptr curList, int value);
+void heap_print(heap_list_ptr heap);
 
 #endif
